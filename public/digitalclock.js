214d2c6a893ba5ca657af1079a8cac2b7092fd8d
kyle <kylescottfox@gmail.com>
2024-03-12 19:20:33 +0000

    cleaning up code

--- public/digitalclock.js
+++ public/digitalclock.js
@@ -2,19 +2,42 @@ class Clock {
   constructor(timeDisplay, dateDisplay) {
     this.timeDisplay = timeDisplay;
     this.dateDisplay = dateDisplay;
-    this.monthsList = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    this.daysList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];  
+    this.monthsList = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    this.daysList = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
     this.date = new Date();
     this.startInterval();
-  };
-  
+  }
+
   //gets the current time and returns formated as hh:mm:ss
   time() {
-    this.hour = (this.date.getHours() < 10 ? '0' : '') + this.date.getHours();
-    this.min = (this.date.getMinutes() < 10 ? '0' : '') + this.date.getMinutes();
-    this.sec = (this.date.getSeconds() < 10 ? '0' : '') + this.date.getSeconds();
+    this.hour = (this.date.getHours() < 10 ? "0" : "") + this.date.getHours();
+    this.min =
+      (this.date.getMinutes() < 10 ? "0" : "") + this.date.getMinutes();
+    this.sec =
+      (this.date.getSeconds() < 10 ? "0" : "") + this.date.getSeconds();
     return `${this.hour}:${this.min}:${this.sec}`;
-  };
+  }
 
   //gets the current date and returns formatted as Weekday | DD Month, YYYY | GMT
   longDateFormat() {
@@ -23,23 +46,23 @@ class Clock {
     this.month = this.monthsList[this.date.getMonth()];
     this.year = this.date.getFullYear();
     return `${this.dayName} | ${this.monthDay} ${this.month} ${this.year} | GMT`;
-  };
+  }
 
   //updates the webpage to reflect the time and longDateFormat method results
   updateDisplay() {
     this.date = new Date();
     this.timeDisplay.textContent = this.time();
     this.dateDisplay.textContent = this.longDateFormat();
-  };
+  }
 
   //calls the updateDisplay method every second so that the webpage keeps updating with the current time
   startInterval() {
     this.updateDisplay();
-    setInterval(() => this.updateDisplay(),1000);
-  };
-};
+    setInterval(() => this.updateDisplay(), 1000);
+  }
+}
 
 //gets the span elements from the index.html file and initializes the Clock class
-const timeDisplay = document.querySelector('.time');
-const dateDisplay = document.querySelector('.date');
+const timeDisplay = document.querySelector(".time");
+const dateDisplay = document.querySelector(".date");
 const clock = new Clock(timeDisplay, dateDisplay);
--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-console.log(__dirname)
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req,res) => {
-  res.render('index.ejs');
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get('/stopwatch', (req,res) => {
-  res.render('stopwatch.ejs');
+app.get("/stopwatch", (req, res) => {
+  res.render("stopwatch.ejs");
 });
 
-app.get('/timersetup', (req,res) => {
-  res.render('timersetup.ejs');
+app.get("/timersetup", (req, res) => {
+  res.render("timersetup.ejs");
 });
 
-app.get('/timer', (req,res) => {
-  res.render('timer.ejs');
+app.get("/timer", (req, res) => {
+  res.render("timer.ejs");
 });
 
-
-app.get('/worldclock', (req,res) => {
-  res.render('worldclock.ejs');
+app.get("/worldclock", (req, res) => {
+  res.render("worldclock.ejs");
 });
 
 app.listen(3000);
